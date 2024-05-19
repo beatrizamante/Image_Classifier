604f2ad68acf1de0abfe33f352168925165c30a7
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-05-19 19:02:26 -0300

    Atividade 11
    Aplicando conceito de two-way-binding

---
+++ my-app/src/app/form/form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  nome: string = '';
+  sobrenome: string = '';
+  endereco: string = '';
+  email: string = '';
+}
