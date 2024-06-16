f4b6f69ea1dc14686eda2dd08179f38d910d4e02
Beatriz <beatrizamante@hotmail.com>
2024-06-16 15:26:38 -0300

    Ajustado DaisyUI e Tailwind, fazendo mais components

---
+++ my-app/src/components/account-screen/account-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-screen.component.html',
+  styleUrl: './account-screen.component.css'
+})
+export class AccountScreenComponent {
+
+}
