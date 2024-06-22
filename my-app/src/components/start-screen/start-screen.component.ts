7992c146a3e605ccee15714dd5a38793836f09ad
iurerw <iurerw@gmail.com>
2024-06-22 18:38:18 -0300

    Adicionado serviço para api rawg + visualização basica do nome dos jogos no tela 'Start-screen'

--- my-app/src/components/start-screen/start-screen.component.ts
+++ my-app/src/components/start-screen/start-screen.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RawgApiService } from '../../app/rawg-api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.css'
 })
-export class StartScreenComponent {
+export class StartScreenComponent implements OnInit{
+  games: any[] = [];
 
+  constructor(private rawgService: RawgApiService) {}
+
+  ngOnInit(): void {
+    this.rawgService.getGames().then(response => {
+      this.games = response.data.results;
+      console.log(this.games);
+    }).catch(error => {
+      console.error('Erro ao carregar jogos', error);
+    });
+  }
 }
