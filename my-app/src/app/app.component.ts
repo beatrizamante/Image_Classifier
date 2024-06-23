a61315cf39f063aa90c8ce8fd385f371accd7319
iurerw <iurerw@gmail.com>
2024-06-23 20:27:18 -0300

    Auto stash before merge of "develop" and "origin/develop"
    Authentication -> funcionando -> login predefinido -> admin senha 'qwerty'

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from '../components/menu/menu.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, NavbarComponent],
+  imports: [RouterOutlet, MenuComponent, NavbarComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
