90b9b54a29b83fd2b06d7f86c35f50ca334529db
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-06-22 23:39:18 -0300

    browse-screen pronto

--- my-app/src/components/browse-screen/browse-screen.component.ts
+++ my-app/src/components/browse-screen/browse-screen.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from "../cards_list/cards.component";
+import { CommonModule } from '@angular/common';
+import { RawgApiService } from '../../app/rawg-api.service';
+import { CardsToRentComponent } from "../cards-to-rent/cards-to-rent.component";
 
 @Component({
-  selector: 'app-browse-screen',
-  standalone: true,
-  imports: [],
-  templateUrl: './browse-screen.component.html',
-  styleUrl: './browse-screen.component.css'
+    selector: 'app-browse-screen',
+    standalone: true,
+    templateUrl: './browse-screen.component.html',
+    styleUrl: './browse-screen.component.css',
+    imports: [CommonModule, CardsToRentComponent]
 })
 export class BrowseScreenComponent {
+  games: any[] = [];
 
+  constructor(private rawgService: RawgApiService) {}
+
+  ngOnInit(): void {
+    this.rawgService.getGames().then((response: { data: { results: any[]; }; }) => {
+      this.games = response.data.results;
+      console.log(this.games)
+    }).catch((error: any) => {
+      console.error('Failed to fetch games', error);
+    });
+  }
 }
+
+
