b7d1a3432fa19e3044d5821176601cdce411ec78
Beatriz <beatrizamante@hotmail.com>
2024-06-23 20:56:28 -0300

    Create account page

---
+++ my-app/src/components/sign-in-screen/sign-in-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in-screen.component.html',
+  styleUrl: './sign-in-screen.component.css'
+})
+export class SignInScreenComponent {
+
+}
