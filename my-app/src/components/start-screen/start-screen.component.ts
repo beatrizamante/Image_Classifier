b1393a4384a0d898029e878769db862953950ff3
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-06-24 19:06:54 -0300

    excluindo componente carrossel e implementando direto no arquivo start-screen

--- my-app/src/components/start-screen/start-screen.component.ts
+++ my-app/src/components/start-screen/start-screen.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RawgApiService } from '../../app/rawg-api.service';
 import { CommonModule } from '@angular/common';
-import { CardsToRentComponent } from "../cards-to-rent/cards-to-rent.component";
 
 @Component({
     selector: 'app-start-screen',
     standalone: true,
     templateUrl: './start-screen.component.html',
     styleUrl: './start-screen.component.css',
-    imports: [CommonModule,CardsToRentComponent]
+    imports: [CommonModule]
 })
 export class StartScreenComponent implements OnInit{
   games: any[] = [];
