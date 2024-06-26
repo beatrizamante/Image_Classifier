74c456ba45c03ce9665ec50d8066549d44c6c911
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-06-26 01:15:01 -0300

    Tela start-screen talvez 99% pronta

--- my-app/src/components/banner/banner.component.ts
+++ my-app/src/components/banner/banner.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
-
+  @Input() game: any;
 }
