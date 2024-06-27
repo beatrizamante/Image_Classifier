5890f570801ab690e0d7e160d3763913bf39d7ac
Beatriz <beatrizamante@hotmail.com>
2024-06-26 21:35:12 -0300

    Tela CouchPlay pronta

--- my-app/src/components/couchplay-screen/couchplay-screen.component.ts
+++ my-app/src/components/couchplay-screen/couchplay-screen.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { CardCouchplayComponent } from "../card-couchplay/card-couchplay.component";
 
 @Component({
-  selector: 'app-couchplay-screen',
-  standalone: true,
-  imports: [],
-  templateUrl: './couchplay-screen.component.html',
-  styleUrl: './couchplay-screen.component.css'
+    selector: 'app-couchplay-screen',
+    standalone: true,
+    templateUrl: './couchplay-screen.component.html',
+    styleUrl: './couchplay-screen.component.css',
+    imports: [FooterComponent, CardCouchplayComponent]
 })
 export class CouchplayScreenComponent {
 
