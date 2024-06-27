094885cc241ae3b8139c9e6b73f71a307154407e
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-06-26 23:23:55 -0300

    couchplay configurada pra consumir a api

--- my-app/src/components/card-couchplay/card-couchplay.component.ts
+++ my-app/src/components/card-couchplay/card-couchplay.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-couchplay',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './card-couchplay.component.css'
 })
 export class CardCouchplayComponent {
+  @Input() game: any;
 
 }
