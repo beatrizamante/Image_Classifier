8095c3bd0750701e46bebe224dde1295bc359c60
Beatriz <beatrizamante@hotmail.com>
2024-06-28 19:30:58 -0300

    Ajustando filtro para dinamicidade de conteúdo

--- my-app/src/components/browse-screen/browse-screen.component.ts
+++ my-app/src/components/browse-screen/browse-screen.component.ts
@@ -3,13 +3,14 @@ import { CardsComponent } from "../cards_list/cards.component";
 import { CommonModule } from '@angular/common';
 import { RawgApiService } from '../../app/rawg-api.service';
 import { CardsToRentComponent } from "../cards-to-rent/cards-to-rent.component";
+import { FilterComponent } from "../filter/filter.component";
 
 @Component({
     selector: 'app-browse-screen',
     standalone: true,
     templateUrl: './browse-screen.component.html',
     styleUrl: './browse-screen.component.css',
-    imports: [CommonModule, CardsToRentComponent]
+    imports: [CommonModule, CardsToRentComponent, FilterComponent]
 })
 export class BrowseScreenComponent {
   games: any[] = [];
--- my-app/src/components/filter/filter.component.ts
+++ my-app/src/components/filter/filter.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
