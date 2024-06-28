1aa9eddd25ca7b41c439d637ad9822b90a28f466
Beatriz <beatrizamante@hotmail.com>
2024-06-28 20:31:25 -0300

    Fixed, for some reason, the problem with z-index and the navbar and menu in the login page

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnDestroy, AfterViewInit } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from '../components/menu/menu.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
@@ -11,17 +12,33 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule ,RouterOutlet, MenuComponent, NavbarComponent, FormsModule],
+  imports: [CommonModule, RouterOutlet, MenuComponent, NavbarComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements AfterViewInit, OnDestroy {
+  private routerEventSubscription!: Subscription;
 
-  constructor(private router: Router) {};
+  constructor(private router: Router, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.routerEventSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.showNavbarAndMenu()) {
+          this.renderer.addClass(document.body, 'with-navbar-menu');
+        } else {
+          this.renderer.removeClass(document.body, 'with-navbar-menu');
+        }
+      }
+    });
+  }
 
   showNavbarAndMenu(): boolean {
-    const noNavRoutes = ['/ ', '/forgot-password-screen', '/sign-in-screen'];
+    const noNavRoutes = ['/', '/forgot-password-screen', '/sign-in-screen'];
     return !noNavRoutes.includes(this.router.url);
-}
+  }
+
+  ngOnDestroy(): void {
+    this.routerEventSubscription.unsubscribe();
+  }
 }
