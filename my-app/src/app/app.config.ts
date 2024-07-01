7f6fdfaa3682a20aafd4a061aeec61d09c1c178a
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-07-01 20:10:53 -0300

    pwa instalado para teste

--- my-app/src/app/app.config.ts
+++ my-app/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app-routing.module';
 import { AuthenticationGuard } from './util/authentication.guard';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), AuthenticationGuard]
+  providers: [provideRouter(routes), AuthenticationGuard, provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
