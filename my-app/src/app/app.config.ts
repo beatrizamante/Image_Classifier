39fa5845d9f80570f95e115b2e3ded07975c1b1c
Beatriz <beatrizamante@hotmail.com>
2024-07-01 19:30:11 -0300

    Feito Submit button não funcionar como eventdefault

--- my-app/src/app/app.config.ts
+++ my-app/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app-routing.module';
 import { AuthenticationGuard } from './util/authentication.guard';
 
